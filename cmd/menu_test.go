529aa80aabde4cacc9ae1efb413d9edb3ef4d989
Harrison Brown <harrybrown98@gmail.com>
2019-03-14 00:10:35 -0700

    writing some preliminary tests for the cmd package

--- cmd/menu_test.go
+++ cmd/menu_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"apizza/pkg/cache"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -16,44 +17,34 @@ func TestRunner(t *testing.T) {
 		return runtime.FuncForPC(reflect.ValueOf(a).Pointer()).Name()
 	}
 
-	defer func() {
-		err := db.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	data, err := db.Get("test")
-	if err != nil {
-		t.Error(err)
-	}
-	if string(data) != "this is some test data" {
-		t.Error("database is broken. go check apizza/pkg/cache tests")
-	}
-	if cfg.Name != "joe" || cfg.Email != "nojoe@mail.com" {
-		t.Error("test data is not correct")
-	}
-
 	var tests = []func(*testing.T){
+		initDummyCheck,
 		testFindProduct,
 	}
 
 	for _, f := range tests {
 		t.Run(funcname(f), f)
 	}
-
-	wd, err := os.Getwd()
-	if err != nil {
-		t.Error("error in cleanup:", err)
-	}
-	err = os.RemoveAll(filepath.Join(wd, "testdata"))
-	if err != nil {
-		t.Error("error in cleanup:", err)
-	}
+	teardownTests()
 }
 
 func testFindProduct(t *testing.T) {
-	t.Error("test not implimented")
+	c := newBuilder().newMenuCmd().(*menuCmd)
+	if err := c.initMenu(); err != nil {
+		t.Error(err)
+	}
+	buf := &bytes.Buffer{}
+	c.output = buf
+
+	c.printMenu()
+
+	// dummy check
+	if len(buf.Bytes()) < 10000 {
+		t.Error("the menu seems to be a bit short in length")
+	}
+	buf.Reset()
+
+	c.printToppings()
 }
 
 func TestStringStuff(t *testing.T) {
@@ -72,11 +63,24 @@ func TestStringStuff(t *testing.T) {
 	}
 }
 
+func initDummyCheck(t *testing.T) {
+	data, err := db.Get("test")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(data) != "this is some test data" {
+		t.Error("database is broken. go check apizza/pkg/cache tests")
+	}
+	if cfg.Name != "joe" || cfg.Email != "nojoe@mail.com" {
+		t.Error("test data is not correct")
+	}
+}
+
 // omg, i can't beleve i haven't been putting this in my other tests, this is great
 func init() {
 	var check = func(e error, msg string) {
 		if e != nil {
-			fmt.Printf("test setup failed: %s: %s\n", e, msg)
+			fmt.Printf("test setup failed: %s - %s\n", e, msg)
 			os.Exit(1)
 		}
 	}
@@ -90,28 +94,24 @@ func init() {
 	err = db.Put("test", []byte("this is some test data"))
 	check(err, "database put")
 
-	raw := `{"Name":"joe","Email":"nojoe@mail.com","Address":{"Street":"","City":"","State":"","Zip":""},"Card":{"Number":"","Expiration":"","CVV":""},"Service":"Carryout","MyOrders":null}`
-	// raw, err := ioutil.ReadFile(filepath.Join(dir, "testconfig"))
-	// check(err, "read config file")
+	raw := `{"Name":"joe","Email":"nojoe@mail.com","Address":{"Street":"1600 Pennsylvania Ave NW","City":"Washington DC","State":"","Zip":"20500"},"Card":{"Number":"","Expiration":"","CVV":""},"Service":"Carryout","MyOrders":null}`
 	err = json.Unmarshal([]byte(raw), cfg)
 	check(err, "json")
 }
 
-func testcleanup() {
-	var check = func(e error, msg string) {
-		if e != nil {
-			fmt.Printf("test cleanup failed: %s: %s\n", e, msg)
-			os.Exit(1)
-		}
+func teardownTests() {
+	if err := db.Close(); err != nil {
+		panic(err)
 	}
 
-	check(db.Close(), "database close")
-
 	wd, err := os.Getwd()
-	check(err, "cwd")
-
-	check(
-		os.RemoveAll(filepath.Join(wd, "testdata")),
-		"delete data",
-	)
+	if err != nil {
+		panic(err)
+	}
+	if err = os.Remove(filepath.Join(wd, "testdata", "test.db")); err != nil {
+		panic(err)
+	}
+	if err = os.Remove(filepath.Join(wd, "testdata")); err != nil {
+		panic(err)
+	}
 }
