8b7b8339bc08ed2e49e15fd37d3689e1bd917843
Harrison Brown <harrybrown98@gmail.com>
2019-04-18 03:28:33 -0700

    moved the order command to the cart.go file

--- cmd/cart.go
+++ cmd/cart.go
@@ -220,3 +220,36 @@ func (b *cliBuilder) newAddOrderCmd() base.CliCommand {
 	c.Flags().StringSliceVarP(&c.toppings, "toppings", "t", c.toppings, "toppings for the products being added")
 	return c
 }
+
+type orderCmd struct {
+	*basecmd
+	verbose bool
+	track   bool
+}
+
+func (c *orderCmd) Run(cmd *cobra.Command, args []string) (err error) {
+	if len(args) < 1 {
+		return data.PrintOrders(db, c.Output(), c.verbose)
+	} else if len(args) > 1 {
+		return errors.New("cannot handle multiple orders")
+	}
+
+	order, err := data.GetOrder(args[0], db)
+	if err != nil {
+		return err
+	}
+
+	if yesOrNo("Would you like to purchase this order? (y/n)") {
+		c.Printf("ordering '%s'...\n", order.Name())
+	}
+	return nil
+}
+
+func newOrderCmd() base.CliCommand {
+	c := &orderCmd{verbose: false}
+	c.basecmd = newCommand("order", "Send an order from the cart to dominos.", c)
+
+	c.Flags().BoolVarP(&c.verbose, "verbose", "v", c.verbose, "output the order command verbosly")
+	c.Flags().BoolVarP(&c.track, "track", "t", c.track, "enable tracking for the purchased order")
+	return c
+}
